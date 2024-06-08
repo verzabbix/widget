a10cded2bd8ad432aee8203ba6c5963d07d8c3b8
Andrejs Verza <andrejs.verza@zabbix.com>
2024-06-08 07:53:45 -0300

    Task 5

--- assets/js/class.widget.js
+++ assets/js/class.widget.js
@@ -1,18 +1,39 @@
 class WMHostIterator extends CWidget {
 
+	displayHosts(hosts) {
+		const list = document.createElement('ul');
+
+		hosts.forEach(host => {
+			const item = document.createElement('li');
+			item.textContent = host.name;
+			list.appendChild(item);
+		});
+
+		this.clearContents();
+
+		this._body.appendChild(list);
+	}
+
 	promiseUpdate() {
-		const {hostgroupids} = this.getFieldsData();
+		const {hostgroupids, hostids} = this.getFieldsData();
 
 		return ApiCall('host.get', {
 			output: ['hostid', 'name'],
 			groupids: hostgroupids.length > 0 ? hostgroupids : undefined,
+			hostids: hostids.length > 0 ? hostids : undefined,
 			filter: {
 				status: 0
 			},
 			sortfield: 'name'
 		})
 			.then(response => {
-				console.log(response.result);
+				const hosts = response.result;
+
+				this.displayHosts(hosts);
 			});
+	}
+
+	hasPadding() {
+		return false;
 	}
 }
